07de8e0df09b6edb5452a039d62dc1fa7c4f533a
Azaray AOS <Azaray AOS>
2017-02-19 16:36:56 +0300

    Добавьте файлы проекта.

---
+++ Provolka/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Provolka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Сопротивление (R, Ом)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(142, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(428, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(142, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(428, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            this.textBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox2_MouseDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Диаметр (d, мм)";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(142, 62);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(428, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Click += new System.EventHandler(this.textBox3_Click);
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            this.textBox3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox3_MouseDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Длина првода (l, м)";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox4.Location = new System.Drawing.Point(142, 88);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(428, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Click += new System.EventHandler(this.textBox4_Click);
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            this.textBox4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox4_MouseDown);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Сечение (S, мм^2)";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Медь (0,0175)",
+            "Алюминий (0,0281)",
+            "Железо (0,135)",
+            "Сталь (0,176)",
+            "Никелин (0,4)",
+            "Константан (0,49)",
+            "Манганин (0,43)",
+            "Нихром (1,1)",
+            "Фехраль (1,3)"});
+            this.comboBox1.Location = new System.Drawing.Point(142, 114);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(130, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 117);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Материал";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 182);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(800, 400);
+            this.MinimumSize = new System.Drawing.Size(277, 200);
+            this.Name = "Form1";
+            this.Text = "Проволка";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Provolka/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Provolka
+{
+    public partial class Form1 : Form
+    {
+        private double p;       // это коэфициент, зависящий от материала
+        private double l;       // Это длинна проводника
+        private double R;       // это сопротивление материала
+        private double S;       // сечение провода
+        private double d;       // диаметр провода
+        public int index;       // показывает, на каком объекте индех
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            p = 0.0175;
+            d = 0.0000000001;
+            S = 0.0000000001;
+            index = 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    p = 0.0175;
+                    break;
+
+                case 1:
+                    p = 0.0281;
+                    break;
+
+                case 2:
+                    p = 0.135;
+                    break;
+
+                case 3:
+                    p = 0.176;
+                    break;
+
+                case 4:
+                    p = 0.4;
+                    break;
+
+                case 5:
+                    p = 0.49;
+                    break;
+
+                case 6:
+                    p = 0.43;
+                    break;
+
+                case 7:
+                    p = 1.1;
+                    break;
+
+                case 8:
+                    p = 1.3;
+                    break;
+            }
+
+            switch (index)
+            {
+                case 1:
+                    Calcualte_R(); ShowData();
+                    break;
+
+                case 3:
+                    Calcualte_l(); ShowData();
+                    break;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (index == 1)
+            {
+                if (textBox1.Text.Length != 0)
+                {
+                    // если вводим сопротивление
+                    R = Convert.ToDouble(textBox1.Text);
+
+                    Calcualte_R();
+                    //textBox3.Text = l.ToString();
+                    //textBox4.Text = S.ToString();
+                    ShowData();
+                }
+                else
+                {
+                    textBox3.Text = "";
+                    textBox4.Text = "";
+                }
+            }
+        }
+
+        private void Calcualte_R()
+        {
+            double a = 0.785 * Math.Pow(d, 2);
+            S = a;
+
+            a = a * R;
+            a = a / p;
+            l = a;
+        }
+
+        private void Calcualte_l()
+        {
+            double a = 0.785 * Math.Pow(d, 2);
+            S = a;
+
+            a = 1.27 * p * l;
+            a = a / Math.Pow(d, 2);
+            R = a;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length != 0)
+                d = Convert.ToDouble(textBox2.Text);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (index == 3)
+            {
+                if (textBox3.Text.Length != 0)
+                {
+                    // если вводим сопротивление
+                    l = Convert.ToDouble(textBox3.Text);
+
+                    Calcualte_l();
+
+                    //textBox1.Text = R.ToString();
+                    //textBox4.Text = S.ToString();
+                    ShowData();
+                }
+                else
+                {
+                    textBox1.Text = "";
+                    textBox4.Text = "";
+                }
+            }
+        }
+
+        private void ShowData()
+        {
+            if (index != 1)
+                textBox1.Text = R.ToString();
+            textBox2.Text = d.ToString();
+            if (index != 3)
+                textBox3.Text = l.ToString();
+            textBox4.Text = S.ToString();
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            index = 2;
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            index = 1;
+        }
+
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            index = 3;
+        }
+
+        private void textBox4_Click(object sender, EventArgs e)
+        {
+            index = 4;
+        }
+
+        private void textBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            index = 2;
+        }
+
+        private void textBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            index = 1;
+        }
+
+        private void textBox3_MouseDown(object sender, MouseEventArgs e)
+        {
+            index = 3;
+        }
+
+        private void textBox4_MouseDown(object sender, MouseEventArgs e)
+        {
+            index = 4;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '.') e.KeyChar = ',';
+
+            if (!(Char.IsDigit(e.KeyChar)) && !((e.KeyChar == ',') &&
+                    (((TextBox)sender).Text.IndexOf(",") == -1) &&
+                    (((TextBox)sender).Text.Length != 0)))
+            {
+                if (e.KeyChar != (char)Keys.Back)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '.') e.KeyChar = ',';
+
+            if (!(Char.IsDigit(e.KeyChar)) && !((e.KeyChar == ',') &&
+                    (((TextBox)sender).Text.IndexOf(",") == -1) &&
+                    (((TextBox)sender).Text.Length != 0)))
+            {
+                if (e.KeyChar != (char)Keys.Back)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '.') e.KeyChar = ',';
+
+            if (!(Char.IsDigit(e.KeyChar)) && !((e.KeyChar == ',') &&
+                    (((TextBox)sender).Text.IndexOf(",") == -1) &&
+                    (((TextBox)sender).Text.Length != 0)))
+            {
+                if (e.KeyChar != (char)Keys.Back)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '.') e.KeyChar = ',';
+
+            if (!(Char.IsDigit(e.KeyChar)) && !((e.KeyChar == ',') &&
+                    (((TextBox)sender).Text.IndexOf(",") == -1) &&
+                    (((TextBox)sender).Text.Length != 0)))
+            {
+                if (e.KeyChar != (char)Keys.Back)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+    }
+}
